133800d8c5842f972a4b20c28ab14cef55e535ef
Gurmandeep Singh <30008833@student.toiohomai.ac.nz>
2019-05-22 11:03:51 +1200

    msg

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class payroll 
+    {
+        static void Main(string[] args)
+        {
+              public static int NumberOfEmployees;
+        private static int salary;
+        private string wages;
+
+        // A read-write instance property:
+        public string 
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        // A read-only static property:
+        public static int wages
+        {
+            get { return wages ; }
+        }
+
+        // A Constructor:
+        public salary ()
+        {
+            // Calculate the employee's payrate :
+            counter = ++Number and wages;
+        }
+    }
+
+    class TestEmployee
+    {
+        static void Main()
+        {
+         
+           
+
+            System.Console.WriteLine(" the number of salary : {0}",
+            System.Console.WriteLine("Employee name: {0}",
+        }
+    }
+   
+
+
+
+
+}
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class payroll
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            {
+                var payCheck = new PayRateCalculator();
+                Console.Write("Please provide the Employees Last Name = salary and wages >>");
+                payCheck.lastName = Console.ReadLine();
+                Console.Write("How many hours has employee {0} worked? >>", payCheck.lastName);
+                payCheck.hours = Convert.ToInt32(Console.ReadLine());
+                Console.Write("What is employee {$33.72}'s hourly wage? >>", payCheck.lastName);
+                payCheck.payrate = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("{0}'s Gross pay is ${1}", payCheck.lastName, payCheck.grossPay);
+                Console.WriteLine("{0}'s Net pay is ${1}", payCheck.lastName, payCheck.netPay);
+            }
+            
+
+         class PayRateCalculator
+        {
+            private float _payrate = 33.72f;
+            private float  _withholdingRate = 0.20F;
+
+            public float hours { get; set; }
+            public float grossPay
+            {
+                get
+                {
+                    return hours * _payrate;
+                }
+            }
+            public float netPay
+            {
+                get
+                {
+                    return grossPay - (grossPay * _withholdingRate);
+                }
+            }
+            public float payrate
+            {
+                get
+                {
+                    return _payrate;
+                }
+                set
+                {
+                    _payrate = value;
+                }
+            }
+            public string lastName { get; set; }
+
+            public PayRateCalculator(string lastName, float hours, float payRate, float salary )
+            {
+                this.hours = hours;
+                this.payrate = payrate;
+                this.lastName = lastName;
+            }
+
+            public PayRateCalculator()
+            {
+                Console.WriteLine(" your anual income is set 80,000");
+                Console.WriteLine(" your wages per week is 1348.80");
+                Console.WriteLine("payrate is $32.72 per hours");
+                Console.WriteLine("payrate calculater");
+
+            }
+
+
+        }
+    }
+}
